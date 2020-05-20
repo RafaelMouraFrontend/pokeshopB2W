b11d6e66bda314a69b1bad68a4e9878a06f04889
Rafael <rafael.mouraweb@gmail.com>
2020-05-19 21:23:15 -0300

    criando chamada na api

--- src/components/PokemonStore/index.js
+++ src/components/PokemonStore/index.js
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 
+import pokemonIndefinifo from "~/assets/indefinido.png";
 import { Container, Card } from "./styles";
 
-const pokemons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function PokemonStore() {
+  const [pokemons, setPokemon] = useState([]);
+  const [image, setImage] = useState([]);
 
-const PokemonStore = () => {
+  useEffect(() => {
+    async function loadPokemon() {
+      const response = await api.get("type/10/");
+
+      const LinkIndex = response.data.pokemon
+        .map((links) => links.pokemon.url)
+        .map((index) => index.split("/")[index.split("/").length - 2]);
+
+      const Image = LinkIndex.map(
+        (id) =>
+          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+      );
+
+      setImage(Image);
+
+      const data = response.data.pokemon.map((pokemon) => ({
+        ...pokemon.pokemon,
+      }));
+
+      setPokemon(data);
+    }
+
+    loadPokemon();
+  }, []);
+
   return (
     <Container>
-      {pokemons.map(() => (
-        <Card>
+      {pokemons.map((pokemon, index) => (
+        <Card key={pokemon.name}>
           <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-            alt="Pikachu"
+            src={image[index] ? image[index] : pokemonIndefinifo}
+            alt={pokemon.name}
           />
-          <p>Nome: Pikachu</p>
+          <p>Nome: {pokemon.name}</p>
           <span>Preço: R$200,00 </span>
         </Card>
       ))}
     </Container>
   );
-};
+}
 export default PokemonStore;
